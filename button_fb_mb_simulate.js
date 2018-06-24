1ada6803c33d1f721577336c99980ac7d3fb999a
Shaon Majumder <smazoomder@gmail.com>
2018-06-24 08:03:38 +0600

    Create button_fb_mb_simulate.js

---
+++ button_fb_mb_simulate.js
@@ -0,0 +1,5 @@
+## Execute any continuous button task on mobile site
+javascript:var inputs = document.getElementsByClassName('button_id'); 
+ for(var i=0; i<inputs.length;i++) { 
+ inputs[i].click(); 
+ }
